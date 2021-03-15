6d920fac40080ee58742456f4fef2a82ecca04d2
Johanthan Kunz <johnathan.kunz@aggiemail.usu.edu>
2021-03-14 21:39:55 -0600

    added log methods to the unit tests

--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
 import unittest
 from ConsumerProgram import consumer
 
+import logging
+
 class consumerTester(unittest.TestCase):
     def test(self):
+        logging.basicConfig(filename='consumerTestLogger.log',level=logging.INFO)
+
+
         fromBucket = "usu-cs5260-bluemoon-requests"
         storageType = "DDB"
         storageName = "widgets"
@@ -12,12 +17,15 @@ class consumerTester(unittest.TestCase):
         self.assertEqual(c.bucketFromName, fromBucket)
         self.assertEqual(c.storageType, storageType)
         self.assertEqual(c.storageName, storageName)
+        logging.info("consumer variables properly set up")
 
         #testing objectGetter
         og = c.getter
         self.assertEqual(og.bucketName, fromBucket)
+        logging.info("getter variables properly set up")
 
         #testing actionDoer
         doer = c.doer
         self.assertEqual(doer.storageName, storageName)
         self.assertEqual(doer.storageType, storageType)
+        logging.info("doer variables properly set up")
